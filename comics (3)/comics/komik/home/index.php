72a08d4ff73b5fba3afb56f58257fdf2caf964ca
aiayay <141732687+aiayay@users.noreply.github.com>
2024-08-08 08:23:47 +0700

    .

--- comics (3)/comics/komik/home/index.php
+++ comics (3)/comics/komik/home/index.php
@@ -22,47 +22,39 @@
               </div>
             </div>
             <!-- ***** Banner End ***** -->
-
+             
             <!-- ***** Most Popular Start ***** -->
             <div class="most-popular">
               <div class="row">
                 <div class="col-lg-12">
-                  <div class="heading-section">
-                    <h4><em>Komik</em> Terbaru</h4>
-                  </div>
-                  <div class="row">
-                            <?php
-                               $komik = mysqli_query($koneksi, "SELECT * FROM komik JOIN user ON komik.id_user=user.id_user ORDER BY id_komik DESC LIMIT 4");
-                                while($data= mysqli_fetch_array($komik)):
-                            ?>
-                            <div class="col-lg-3 col-sm-6">
-                                <div class="item">
-                                    <a href="?page=komik/detail_komik&id_komik=<?php echo $data['id_komik'];?>"><img class="img-fluid" src="admin/assets/images/cover/<?= $data['cover']?>" alt="" width="100px" height="100px"></a>
-                                  <h4><?= $data['judul']; ?><br /><span><?= $data['nama_lengkap']?></span></h4>
-                                </div>
+                  <!-- ***** Featured Games Start ***** -->
+                    <div class="row">
+                        <div class="col-lg-12">
+                          <div class="featured-games header-text">
+                            <div class="heading-section">
+                              <h4><em>Komik</em> Terbaru</h4>
                             </div>
-                          <?php endwhile; ?>
-                          <?php
-                                          $nextKomik = mysqli_query($koneksi, "SELECT * FROM komik JOIN user ON komik.id_user=user.id_user ORDER BY id_komik DESC LIMIT 4");
-                                          $next = mysqli_fetch_array($nextKomik);
-                                          
-                                          // Ambil ID komik sebelumnya
-                                          $prevKomik = mysqli_query($koneksi, "SELECT * FROM komik JOIN user ON komik.id_user=user.id_user ORDER BY id_komik DESC LIMIT 4");
-                                          $back = mysqli_fetch_array($prevKomik);
-                                          ?>
-                          <div class="col-lg-12 d-flex justify-content-between">
-                                      <?php if ($back) : ?>
-                                        <div class="main-button">
-                                          <a href="#">Back</a>
-                                        </div>
-                                      <?php endif; ?>
-                                      <?php if ($next) : ?>
-                                        <div class="main-button">
-                                          <a href="#">Next</a>
-                                        </div>
-                                      <?php endif; ?>
-                                    </div>
-                  </div>
+                            <div class="owl-features owl-carousel">
+                            <?php
+                                          $komik = mysqli_query($koneksi, "SELECT * FROM komik JOIN user ON komik.id_user=user.id_user ORDER BY id_komik DESC LIMIT 4");
+                                            while($data= mysqli_fetch_array($komik)):
+                                        ?>
+                              <div class="item">
+                                <div class="thumb">
+                                  <a href="?page=komik/detail_komik&id_komik=<?php echo $data['id_komik']; ?>"><img src="admin/assets/images/cover/<?= $data['cover'] ?>" alt=""></a>
+                                  <div class="hover-effect">
+                                    <a href="?page=komik/detail_komik&id_komik=<?php echo $data['id_komik']; ?>"><h6><?= $data['deskripsi']; ?></h6></a>
+                                  </div>
+                                </div>
+                                    <a href="?page=komik/detail_komik&id_komik=<?php echo $data['id_komik']; ?>"><h4><?= $data['judul']; ?><br><span><?= $data['nama_lengkap']?></span></h4></a>
+                              </div>
+                              <?php endwhile; ?>
+                            </div>
+                          </div>
+                        </div>
+                      
+                      </div>
+          <!-- ***** Featured Games End ***** -->
                 </div>
               </div>
             </div>
@@ -105,4 +97,4 @@
     </div>
   </div>
 
-
+ 
